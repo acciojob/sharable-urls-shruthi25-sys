f2838617ec64e16bda1a1a0988481190e683bb18
Suruthika Ramesh <manimala1614@gmail.com>
2026-04-21 23:22:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-// your code here
+const form=document.getElementById("form");
+const nameInput =document.getElementById("name");
+const yearInput=document.getElementById("year");
+const urlText=document.getElementById("url");
+form.addEventListener ("submit",function(event)){
+event.preventDefault();
+let baseUrl="https://localhost:8080/";
+let params=[];
+if(nameInput.value.trim()!==""){
+params.push("name=" + encodeURIComponent(nameInput.value));
+}
+if(yearInput.value.trim()!==""){
+params.push("year=" + encodeURIComponent(yearInput.value));
+
+}
+if(params.length>0){
+baseUrl += "?" + params.join("&");
+} 
+urlText.textContent=baseUrl;
+
+});
+
+
+
+
