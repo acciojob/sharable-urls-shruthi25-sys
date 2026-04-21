fa8e7a0a178f55acdfdf5b2093c035ae7ef3014e
Suruthika Ramesh <manimala1614@gmail.com>
2026-04-21 23:33:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const form=document.getElementById("form");
 const nameInput =document.getElementById("name");
 const yearInput=document.getElementById("year");
 const urlText=document.getElementById("url");
-form.addEventListener ("submit",function(event)){
+form.addEventListener ("submit",function(event){
 event.preventDefault();
 let baseUrl="https://localhost:8080/";
 let params=[];
